74314fa2ab52f30e96066474b94f748aa86b6bd3
abhinavonweb <thisisfordevelopment322@gmail.com>
2022-10-08 20:00:31 +0530

    my first production relase

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const format = require("date-format");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("<h1>Hello from LCO</h1>");
+  });
+
+
+
+ app.get("/api/v1/instagram", (req, res) => {
+    const instaSocial = {
+      username: "abhinavmishra",
+      folowers: 66,
+      follows: 70,
+      date: format.asString("dd[MM][yyyy] - hh:mm:ss", new Date()),
+    };
+  
+    res.status(200).json(instaSocial);
+  });
+
+
+  app.get("/api/v1/facebook", (req, res) => {
+    const instaSocial = {
+      username: "abhinavmishra",
+      folowers: 88,
+      follows: 10,
+      date: format.asString("dd[MM] - hh:mm:ss", new Date()),
+    };
+  
+    res.status(200).json(instaSocial);
+  });
+  
+  app.get("/api/v1/linkedin", (req, res) => {
+    const instaSocial = {
+      username: "abhinavonweb",
+      folowers: 800,
+      follows: 80,
+      date: new Date(),
+    };
+
+     
+    res.status(200).json(instaSocial);
+  });
+  
+
+
+
+  app.get("/api/v1/:token", (req, res) => {
+    console.log(req.params.token);
+    res.status(200).json({ param: req.params.token });
+  });
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`);
+  });
